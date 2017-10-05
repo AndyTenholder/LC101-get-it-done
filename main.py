0125278ea933c5328cd863e622f13c58c3bd33cf
Andy Tenholder <andytenholder@gmail.com>
2017-10-05 15:07:42 -0500

    Connected to MAMP database using SQLAlchemy

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+# Note: the connection string after :// contains the following info:
+# user:password@server:portNumber/databaseName
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://git-it-done:password@localhost:8889/git-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
+
 
 tasks = []
 
@@ -14,5 +31,5 @@ def index():
 
     return render_template('todos.html',title="Get It Done!", tasks=tasks)
 
-
-app.run()
+if __name__ == '__main__':
+    app.run()
